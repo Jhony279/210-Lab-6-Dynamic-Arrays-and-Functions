639923b2565dbe25ca394f68ce4a46b46cd6f0d7
Jhony279 <lordjhony732@gmail.comm>
2026-02-12 15:15:11 -0800

    Initial test to make sure everything is working and creating an array to insert into the first function

--- main.cpp
+++ main.cpp
@@ -9,19 +9,29 @@ void enterArrayData(int[]);
 int outputArrayData();
 int sumArray();
 
+const int SIZE = 12;
+
 /**
  * @brief - Description of main
  * @return 
 */
 int main() {
+    int *arr = nullptr;
+    arr = new int[SIZE];
+
     return 0;
 }
 
-void enterArrayData(int[]){
+void enterArrayData(int dArray[]){
     const int spacing = 4;
+    int temp = 0;
+    int *ptrArray = dArray;
 
     cout << "Data entry for the array" << endl;
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < SIZE; i++){
         cout << setw(spacing) << "> Element #" << i <<": ";
+        cin >> temp;
+        ptrArray[i] = temp;
+
     }
 }
